0ddd9d97767015a5850d2eb1fc51c8c84a3fab7d
sreekara <sreedansd@gmail.com>
2024-06-09 21:35:28 -0400

    feat(auth): added a small auth check to ensure third parties can't control my playback

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::sync::Arc;
-use axum::{body::Body, http::Response, routing::get, Extension, Router};
+use axum::{body::Body, extract::Query, http::Response, routing::get, Extension, Router};
 use chrono::{Duration, TimeDelta};
+use dotenv::dotenv;
+use once_cell::sync::Lazy;
 use rspotify::{
     clients::{BaseClient, OAuthClient},
     model::{AdditionalType, Device, FullTrack, RepeatState},
     scopes, AuthCodeSpotify, Credentials, Token,
 };
-use serde::Serialize;
-use tokio::sync::Mutex; 
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(Debug, Deserialize)]
+struct QueryParams {
+    auth_token: String,
+}
 
 #[derive(Debug, Clone)]
 struct PlaybackState {
@@ -66,9 +73,14 @@ impl Track {
     }
 }
 
+// Usually, I would implement an OAuth system with access tokens to make it more secure, but this
+// is so small scale that I'm fine with having a token I can just recreate if need be.
+static AUTH_TOKEN: Lazy<Arc<Mutex<String>>> =
+    Lazy::new(|| Arc::new(Mutex::new(dotenv::var("AUTH_TOKEN").unwrap())));
+
 async fn update_state(state: Extension<Arc<Mutex<SpotifyState>>>) {
     let mut locked_state = state.lock().await;
-    
+
     let currently_playing_res = locked_state
         .spotify
         .current_playback(
@@ -77,27 +89,26 @@ async fn update_state(state: Extension<Arc<Mutex<SpotifyState>>>) {
         )
         .await;
 
-        match currently_playing_res {
-            Ok(Some(playing)) => {
-                locked_state.playback_status = Some(PlaybackState {
-                   is_playing: playing.is_playing,
-                   position: playing.progress.unwrap().num_seconds() as u64,
-                   device_id: playing.device.clone().id.unwrap(),
-                   
-                });
-            }
-            Ok(None) => {
-                locked_state.playback_status = None;
-            }
-            Err(_) => {
-                locked_state.playback_status = None;
-            }
+    match currently_playing_res {
+        Ok(Some(playing)) => {
+            locked_state.playback_status = Some(PlaybackState {
+                is_playing: playing.is_playing,
+                position: playing.progress.unwrap().num_seconds() as u64,
+                device_id: playing.device.clone().id.unwrap(),
+            });
         }
-
-
+        Ok(None) => {
+            locked_state.playback_status = None;
+        }
+        Err(_) => {
+            locked_state.playback_status = None;
+        }
+    }
 }
 
-async fn get_current_playback(state: Extension<Arc<Mutex<SpotifyState>>>) -> Result<Response<Body>, String> {
+async fn get_current_playback(
+    state: Extension<Arc<Mutex<SpotifyState>>>,
+) -> Result<Response<Body>, String> {
     let mut locked_state = state.lock().await;
 
     // TODO: In attempts to not call spotify api as often, make it so it only updates every 5 seconds
@@ -113,7 +124,8 @@ async fn get_current_playback(state: Extension<Arc<Mutex<SpotifyState>>>) -> Res
     match currently_playing_res {
         Ok(Some(playing)) => {
             let track_info = match playing.item.unwrap().id().unwrap() {
-                rspotify::model::PlayableId::Track(track_id) => locked_state.spotify
+                rspotify::model::PlayableId::Track(track_id) => locked_state
+                    .spotify
                     .track(track_id, None)
                     .await
                     .expect("Could not get information for track"),
@@ -124,9 +136,9 @@ async fn get_current_playback(state: Extension<Arc<Mutex<SpotifyState>>>) -> Res
             };
 
             locked_state.playback_status = Some(PlaybackState {
-               is_playing: playing.is_playing,
-               position: playing.progress.unwrap().num_seconds() as u64,
-               device_id: playing.device.clone().id.unwrap(),
+                is_playing: playing.is_playing,
+                position: playing.progress.unwrap().num_seconds() as u64,
+                device_id: playing.device.clone().id.unwrap(),
             });
 
             let res_playing = CurrentlyPlaying {
@@ -157,13 +169,27 @@ async fn get_current_playback(state: Extension<Arc<Mutex<SpotifyState>>>) -> Res
         }
         Err(err) => {
             locked_state.playback_status = None;
-            
+
             Err(format!("Error with getting playback, {}", err))
         }
     }
 }
 
-async fn toggle_playback(state: Extension<Arc<Mutex<SpotifyState>>>) {
+async fn toggle_playback(
+    Query(params): Query<QueryParams>,
+    state: Extension<Arc<Mutex<SpotifyState>>>,
+) -> Result<Response<Body>, String> {
+    let locked_token = AUTH_TOKEN.lock().await;
+    let auth_token = params.auth_token;
+
+    if locked_token.as_str() != auth_token {
+        return Ok(Response::builder()
+            .status(400)
+            .header("Content-Type", "application/json")
+            .body(Body::new("{\"message\": \"invalid token\"}".to_string()))
+            .unwrap());
+    }
+
     update_state(state.clone()).await;
 
     let locked_state = state.lock().await;
@@ -172,47 +198,136 @@ async fn toggle_playback(state: Extension<Arc<Mutex<SpotifyState>>>) {
         if playback.is_playing {
             let _ = locked_state
                 .spotify
-                .pause_playback(Some(playback.device_id.as_str())).await;
+                .pause_playback(Some(playback.device_id.as_str()))
+                .await;
             playback.is_playing = false;
+
+            return Ok(Response::builder()
+                .status(200)
+                .header("Content-Type", "application/json")
+                .body(Body::new("{\"message\": \"playback paused\"}".to_string()))
+                .unwrap());
         } else {
             let _ = locked_state
                 .spotify
                 .resume_playback(
-                    Some(playback.device_id.as_str()), 
-                    Some(TimeDelta::new(playback.position as i64, 0).unwrap())
-                ).await;
-            
+                    Some(playback.device_id.as_str()),
+                    Some(TimeDelta::new(playback.position as i64, 0).unwrap()),
+                )
+                .await;
+
             playback.is_playing = false;
+
+            return Ok(Response::builder()
+                .status(200)
+                .header("Content-Type", "application/json")
+                .body(Body::new("{\"message\": \"playback resumed\"}".to_string()))
+                .unwrap());
         }
+    } else {
+        return Err("Could not toggle playback".to_string());
     }
 }
 
-async fn next_track(state: Extension<Arc<Mutex<SpotifyState>>>) {
+async fn next_track(
+    Query(params): Query<QueryParams>,
+    state: Extension<Arc<Mutex<SpotifyState>>>,
+) -> Result<Response<Body>, String> {
+    let locked_token = AUTH_TOKEN.lock().await;
+    let auth_token = params.auth_token;
+    if locked_token.as_str() != auth_token {
+        return Ok(Response::builder()
+            .status(400)
+            .header("Content-Type", "application/json")
+            .body(Body::new("{\"message\": \"invalid token\"}".to_string()))
+            .unwrap());
+    }
+
     update_state(state.clone()).await;
-        
+
     let locked_state = state.lock().await;
     let device_id = locked_state.clone().playback_status.unwrap().device_id;
-    let _ = locked_state.spotify.next_track(Some(device_id.as_str())).await;
+    let _ = locked_state
+        .spotify
+        .next_track(Some(device_id.as_str()))
+        .await;
+
+    return Ok(Response::builder()
+        .status(200)
+        .header("Content-Type", "application/json")
+        .body(Body::from(
+            "{\"message\": \"successfully skipped to the next track\"".to_string(),
+        ))
+        .unwrap());
 }
 
-async fn previous_track(state: Extension<Arc<Mutex<SpotifyState>>>) {
+async fn previous_track(
+    Query(params): Query<QueryParams>,
+    state: Extension<Arc<Mutex<SpotifyState>>>,
+) -> Result<Response<Body>, String> {
+    let locked_token = AUTH_TOKEN.lock().await;
+    let auth_token = params.auth_token;
+    if locked_token.as_str() != auth_token {
+        return Ok(Response::builder()
+            .status(400)
+            .header("Content-Type", "application/json")
+            .body(Body::new("{\"message\": \"invalid token\"}".to_string()))
+            .unwrap());
+    }
+
     update_state(state.clone()).await;
-    
+
     let locked_state = state.lock().await;
     let device_id = locked_state.clone().playback_status.unwrap().device_id;
-    let _ = locked_state.spotify.previous_track(Some(device_id.as_str())).await;
+    let _ = locked_state
+        .spotify
+        .previous_track(Some(device_id.as_str()))
+        .await;
+
+    return Ok(Response::builder()
+        .status(200)
+        .header("Content-Type", "application/json")
+        .body(Body::from(
+            "{\"message\": \"successfully skipped to the previous track\"".to_string(),
+        ))
+        .unwrap());
 }
 
-async fn restart_track(state: Extension<Arc<Mutex<SpotifyState>>>) {
+async fn restart_track(
+    Query(params): Query<QueryParams>,
+    state: Extension<Arc<Mutex<SpotifyState>>>,
+) -> Result<Response<Body>, String> {
+    let locked_token = AUTH_TOKEN.lock().await;
+    let auth_token = params.auth_token;
+    if locked_token.as_str() != auth_token {
+        return Ok(Response::builder()
+            .status(400)
+            .header("Content-Type", "application/json")
+            .body(Body::new("{\"message\": \"invalid token\"}".to_string()))
+            .unwrap());
+    }
+
     update_state(state.clone()).await;
 
     let locked_state = state.lock().await;
     let device_id = locked_state.clone().playback_status.unwrap().device_id;
-    let _ = locked_state.spotify.seek_track(Duration::seconds(0), Some(device_id.as_str())).await;
+    let _ = locked_state
+        .spotify
+        .seek_track(Duration::seconds(0), Some(device_id.as_str()))
+        .await;
+
+    return Ok(Response::builder()
+        .status(200)
+        .header("Content-Type", "application/json")
+        .body(Body::from(
+            "{\"message\": \"successfully restarted the track\"".to_string(),
+        ))
+        .unwrap());
 }
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let creds = Credentials::from_env().unwrap();
     let mut spotify = AuthCodeSpotify::from_token(Token {
         refresh_token: Some(std::env::var("REFRESH_TOKEN").unwrap()),
